c159205fb5c5a97d9755205433ea698758153898
경빈 박 <rudqls1123@naver.com>
2023-04-21 19:37:30 +0900

    feat : Stack

---
+++ 04. Stack/Adapter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{ 
+    internal class AdapterStack<T>
+    {
+        private List<T> container;                      // 어뎁터스택의 필드 일반화 리스트
+
+        public AdapterStack()                           // 생성자
+        {
+            container = new List<T>();                  // 컨테이너 리스트 인스턴스 생성
+        }
+        public void Push(T item)                        // push 메서드 구현
+        {
+            container.Add(item);                        // 리스트의 Add와 기능이 같음
+        }
+        public T Pop()                                  // Pop 메서드 구현
+        {
+            T item = container[container.Count - 1];    // 지역변수 아이템에 리스트 마지막 값을 담음
+            container.RemoveAt(container.Count - 1);    // 마지막 값 지움
+            return item;                                
+        }
+        public T Peek()
+        {
+            return container[container.Count - 1];
+        }
+    }
+}
---
+++ 04. Stack/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _04._Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
